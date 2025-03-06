8ade10480574fd8af6223453d2f8e9ac81e2c368
vive1235 <vipsakamani141100@gmail.com>
2025-03-06 16:36:28 -0700

    Backend Authorization: create a user each user can access their own notes space. if try to access other user's space that not allowed

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,7 @@
 # This serializer is used to convert Django's User model into JSON format for API responses and handle user registration.
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data) # Ensures password hashing
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
